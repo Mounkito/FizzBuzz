5b9c5ab71f5b7ce3e13e7bf1bad3e50b10fd8d84
Yohan <yohanlamerand@gmail.com>
2017-09-06 11:44:24 +0200

    [#01] Adding : Fizz test with number 6

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -4,8 +4,14 @@ import org.junit.Test;
 
 public class FizzBuzzTest {
     @Test
-    public void should_return_fizz() throws Exception {
+    public void should_return_fizz_when_the_given_number_is_three() throws Exception {
         FizzBuzz fizzBuzz = new FizzBuzz();
         Assertions.assertThat(fizzBuzz.generate(3)).isEqualTo("Fizz");
+    }
+
+    @Test
+    public void should_return_fizz_when_the_given_number_is_six() throws Exception {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        Assertions.assertThat(fizzBuzz.generate(6)).isEqualTo("Fizz");
     }
 }
