e47e2e780f483a83ec848e05979b184de48ce74e
Yohan <yohanlamerand@gmail.com>
2017-09-06 11:50:43 +0200

    [##] Refactor  : Set Up method

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,29 +1,34 @@
 import domain.FizzBuzz;
 import org.assertj.core.api.Assertions;
+import org.junit.Before;
 import org.junit.Test;
 
 public class FizzBuzzTest {
+
+    private FizzBuzz fizzBuzz;
+
+    @Before
+    public void setUp() throws Exception {
+        fizzBuzz = new FizzBuzz();
+    }
+
     @Test
     public void should_return_fizz_when_the_given_number_is_three() throws Exception {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         Assertions.assertThat(fizzBuzz.generate(3)).isEqualTo("Fizz");
     }
 
     @Test
     public void should_return_fizz_when_the_given_number_is_six() throws Exception {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         Assertions.assertThat(fizzBuzz.generate(6)).isEqualTo("Fizz");
     }
 
     @Test
     public void should_return_buzz_when_the_given_number_is_five() throws Exception {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         Assertions.assertThat(fizzBuzz.generate(5)).isEqualTo("Buzz");
     }
 
     @Test
     public void should_return_buzz_when_the_given_number_is_ten() throws Exception {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         Assertions.assertThat(fizzBuzz.generate(10)).isEqualTo("Buzz");
     }
 }
